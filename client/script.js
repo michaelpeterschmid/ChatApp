a7656c75e0bb1f72be8c9ea3d36f352e57443f9c
Schmid Michael Peter <michaelpeter.schmid17@gmail.com>
2025-08-15 16:17:39 +0200

    Initial Commit

---
+++ client/script.js
@@ -0,0 +1,70 @@
+const joinRoomButton = document.getElementById("room-button");
+const messageInput = document.getElementById("message-input");
+const roomInput = document.getElementById("room-input");
+const form = document.getElementById("form");
+
+
+
+const socket = io("http://localhost:3000");
+
+// listening to event coming down from the server
+socket.on("connect", () => {
+    displayMessage(`You connected with id: ${socket.id}`)
+})
+
+socket.on("receive-message", message => {
+    displayMessage(message);
+})
+
+
+
+
+
+
+form.addEventListener("submit", event => {
+    /*The preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
+
+    For example, this can be useful when:
+
+    Clicking on a "Submit" button, prevent it from submitting a form
+    Clicking on a link, prevent the link from following the UR */
+    event.preventDefault()
+
+    const message = messageInput.value;
+    const room = roomInput.value;
+    console.log(room);
+
+
+    if(message === "" ) {
+        return;
+    }else{
+        displayMessage(message);
+
+        socket.emit("send-message", message, room);
+        messageInput.value = ""; //clear out the input
+    }
+
+})
+
+
+
+
+joinRoomButton.addEventListener("click", () => {
+    const room = roomInput.value;
+
+
+    //say to the server that we want to join to a specific room
+    socket.emit("join-room", room, /*callback function at last to inform user about success*/ message => {
+        displayMessage(message);
+    })
+})
+
+
+
+function displayMessage(message){
+    const div = document.createElement("div");
+    div.classList.add("messagediv");
+    const br = document.createElement("br");
+    div.textContent = message;
+    document.getElementById("message-container").append(div, br);
+}
---
+++ server/server.js
@@ -0,0 +1,59 @@
+// server.js
+import http from "http";
+import express from "express";
+import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+
+// Serve ../client  (your HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, "../client")));
+
+
+
+// Create HTTP server
+const httpServer = http.createServer(app);
+
+// Create WebSocket server on top of HTTP
+const io = new Server(httpServer);
+
+
+//runs every time a client connects to our server, and gives each one an id (it's room)
+io.on("connection", (socket) => {
+
+  console.log("A player connected:", socket.id);
+
+
+  //listening for events from client!
+  socket.on("send-message", (message, room) => {
+
+    //and calling events in all clients (indlucing the one that sent the message)
+    /* io.emit("receive-message", message); */
+
+    //broadcasting if no room mentioned, otherwise emiting to specific room
+    if(room===""){
+        //just calling events in all clients but not the one that send it:
+        socket.broadcast.emit("receive-message", message);
+    } else {
+        socket.to(room).emit("receive-message", message);
+    }
+
+    console.log(message);
+  })
+
+  socket.on("join-room", (room, callback) => {
+    socket.join(room);
+    callback(`you joined the room: ${room}`);
+  })
+
+});
+
+// Start HTTP server
+httpServer.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
